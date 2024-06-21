a1a2ce37ba9185dbc26ba3bc30b6b93e19381b9f
豐民 廖 <publicstatic5665@gmail.com>
2024-06-21 16:19:55 +0800

    Feat: 新增Product和Home Controller

---
+++ ProductSideproject/ProductSideproject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductSideproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Redirect("https://www.netflix.com/browse");
+        }
+        public ActionResult FirstAction() 
+        {
+            return View();
+        }
+        public ActionResult GotoProduct() 
+        {
+            //Operation
+            //return RedirectToAction("Index");
+            return RedirectToAction("Index","Product");
+        }
+        public ActionResult GotoUpdateProduct() 
+        {
+            return RedirectToAction("UpdateProduct", "Product", new {ID = 20});
+        
+        }
+    }
+}
