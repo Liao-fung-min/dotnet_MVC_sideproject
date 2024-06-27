dd854ead0dc9a89a228f8f4190cf3e49c5a4c09e
豐民 廖 <publicstatic5665@gmail.com>
2024-06-27 21:52:42 +0800

    Fetch: AddEmployee View

---
+++ ProductSideproject/ProductSideproject/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using ProductSideproject.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ProductSideproject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> employeelist = new List<Employee>();
+            Employee employee1 = new Employee
+            {
+                ID = 1,
+                Name = "Alex",
+                Salary = 4000
+            };
+            employeelist.Add(employee1);
+            Employee employee2 = new Employee
+            {
+                ID = 2,
+                Name = "Liao",
+                Salary = 4399
+            };
+            employeelist.Add(employee2);
+            return View(employeelist);
+        }
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+        public ActionResult UpdateEmployee()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProductSideproject/ProductSideproject/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductSideproject.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
